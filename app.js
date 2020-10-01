3e2cbb49816a7468f73961eb114aa71a6819c54e
Kabirdas Henry <kbrdsk@gmail.com>
2020-10-01 16:14:34 -0700

    initial commit

---
+++ app.js
@@ -0,0 +1,46 @@
+const express = require("express");
+const path = require("path");
+const cookieParser = require("cookie-parser");
+const logger = require("morgan");
+const sassMiddleware = require("node-sass-middleware");
+
+const inventoryRouter = require("./routes/inventory");
+
+const app = express();
+
+const mongoose = require("mongoose");
+const dev_db_url = require("./db_url");
+const mongoDB = process.env.MONGODB_URI || dev_db_url;
+mongoose.connect(mongoDB, { useNewUrlParser: true, useUnifiedTopology: true });
+const db = mongoose.connection;
+db.on("error", console.error.bind(console, "MongoDB connection error:"));
+
+app.set("views", _dirname + "/views");
+app.set("view engine", "jsx");
+app.engine("jsx", require("express-react-views").createEngine());
+
+app.use(logger("dev"));
+app.use(express.json());
+app.use(express.urlencoded({ extended: false }));
+app.use(cookieParser());
+app.use(
+	sassMiddleware({
+		src: path.join(__dirname, "public"),
+		dest: path.join(__dirname, "public"),
+		indentedSyntax: false, // true = .sass and false = .scss
+		sourceMap: true,
+	})
+);
+app.use(express.static(path.join(__dirname, "public")));
+
+app.use("/inventory", inventoryRouter);
+
+app.get("/", (req, res) => {
+	res.redirect("/inventory");
+});
+
+app.use((req, res, next) => {
+	next(createError(404));
+});
+
+module.exports = app;
---
+++ views/default.jsx
@@ -0,0 +1,58 @@
+const React = require("react");
+
+function DefaultLayout(props) {
+	return (
+		<html>
+			<head>
+				<meta charset="utf8" />
+				<title>{props.title}</title>
+			</head>
+			<body>
+				<h1>{props.children}</h1>
+				<NavBar />
+				{props.children}
+			</body>
+		</html>
+	);
+}
+
+function NavBar(props) {
+	return (
+		<nav>
+			<ul className="sidebar-nav">
+				<li>
+					<a href="/inventory">Home</a>
+				</li>
+				<li>
+					<a href="/inventory/produce">Produce</a>
+				</li>
+				<li>
+					<a href="/inventory/protein">Protein</a>
+				</li>
+				<li>
+					<a href="/inventory/protein">Carbs</a>
+				</li>
+				<li>
+					<a href="/inventory/spices">Spices</a>
+				</li>
+				<li>
+					<hr>
+				</li>
+				<li>
+					<a href="/inventory/produce/create">Create New Produce</a>
+				</li>
+				<li>
+					<a href="/inventory/protein/create">Create New Protein</a>
+				</li>
+				<li>
+					<a href="/inventory/carb/create">Create New Carb</a>
+				</li>
+				<li>
+					<a href="/inventory/spice/create">Create New Spice</a>
+				</li>
+			</ul>
+		</nav>
+	);
+}
+
+module.exports = DefaultLayout;
