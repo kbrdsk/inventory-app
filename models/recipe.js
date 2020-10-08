70009e8f11826bf63ee62118d6a77411cebd2792
Kabirdas Henry <kbrdsk@gmail.com>
2020-10-08 09:40:13 -0700

    stores item/amount properly using subdocuments

--- models/recipe.js
+++ models/recipe.js
@@ -1,10 +1,15 @@
 const mongoose = require("mongoose");
 const Schema = mongoose.Schema;
 
+const RecipeItemSchema = new Schema({
+	item: { type: Schema.Types.ObjectId, ref: "Item", required: true },
+	amount: { type: String, required: true },
+});
+
 const RecipeSchema = new Schema({
 	name: { type: String, required: true, minlength: 3, maxlength: 100 },
-	ingredients: [{ type: Schema.Types.ObjectId, ref: "Item" }],
-	instructions: [{ type: String }],
+	ingredients: [RecipeItemSchema],
+	instructions: [String],
 	prep_time: { type: Number },
 	cooking_time: { type: Number },
 	vegan: { type: Boolean, required: true },
